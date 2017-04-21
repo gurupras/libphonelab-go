695103061f361666cc92398edae3ff47c69e4021
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2017-04-21 02:02:17 -0400

    Updated missing_loglines.go

--- missing_loglines.go
+++ missing_loglines.go
@@ -1,18 +1,12 @@
 package libphonelab
 
 import (
-	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
 	"sync"
 
-	"github.com/gurupras/go-easyfiles"
 	"github.com/shaseley/phonelab-go"
-	"github.com/shaseley/phonelab-go/hdfs"
 )
 
 type MissingLoglinesProcGenerator struct{}
@@ -35,24 +29,18 @@ type MissingChunk struct {
 	End   int64 `json:"end"`
 }
 type MissingLoglinesResult struct {
-	BasePath string `json:"base_path"`
-	DeviceId string `json:"deviceid"`
-	BootId   string `json:"bootid"`
-	Start    int64  `json:"start"`
-	End      int64  `json:"end"`
-	hdfsAddr string
-	Missing  []*MissingChunk `json:"missing"`
+	Info    phonelab.PipelineSourceInfo
+	Start   int64           `json:"start"`
+	End     int64           `json:"end"`
+	Missing []*MissingChunk `json:"missing"`
 }
 
 func (p *MissingLoglinesProcessor) Process() <-chan interface{} {
 	outChan := make(chan interface{})
 
 	result := &MissingLoglinesResult{}
-	result.DeviceId = p.Info["deviceid"].(string)
-	result.BootId = p.Info["bootid"].(string)
-	result.BasePath = p.Info["basePath"].(string)
-	result.hdfsAddr = p.Info["hdfsAddr"].(string)
 	result.Missing = make([]*MissingChunk, 0)
+	result.Info = p.Info
 
 	go func() {
 		defer close(outChan)
@@ -90,72 +78,76 @@ func (p *MissingLoglinesProcessor) Process() <-chan interface{} {
 
 type MissingLoglinesCollector struct {
 	sync.Mutex
-	Data     []*MissingLoglinesResult
-	DeviceId string
-	BootId   string
-	BasePath string
-	hdfsAddr string
+	Data []*MissingLoglinesResult
 }
 
 func (c *MissingLoglinesCollector) OnData(data interface{}) {
-	r := data.(*MissingLoglinesResult)
-	c.Lock()
-	defer c.Unlock()
-	if strings.Compare(c.DeviceId, "") == 0 {
-		c.DeviceId = r.DeviceId
-		c.BootId = r.BootId
-		c.BasePath = r.BasePath
-		c.hdfsAddr = r.hdfsAddr
-	}
-	c.Data = append(c.Data, r)
+	// FIXME: Fix this once collector is finalized
+	/*
+		r := data.(*MissingLoglinesResult)
+		c.Lock()
+		defer c.Unlock()
+		if strings.Compare(c.DeviceId, "") == 0 {
+			c.DeviceId = r.DeviceId
+			c.BootId = r.BootId
+			c.BasePath = r.BasePath
+			c.hdfsAddr = r.hdfsAddr
+		}
+		c.Data = append(c.Data, r)
+	*/
 }
 
 func (c *MissingLoglinesCollector) Finish() {
-	deviceId := c.DeviceId
-	basePath := c.BasePath
-	outdir := filepath.Join(basePath, deviceId, "analysis")
+	// FIXME: Fix this once collector is finalized
+	/*
+		deviceId := c.DeviceId
+		basePath := c.BasePath
+		outdir := filepath.Join(basePath, deviceId, "analysis")
 
-	client, err := hdfs.NewHdfsClient(c.hdfsAddr)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to get hdfs client: %v", err))
-	}
-	if client != nil {
-		err = client.MkdirAll(outdir, 0775)
-	} else {
-		if !easyfiles.Exists(outdir) {
-			err = easyfiles.Makedirs(outdir)
+		client, err := hdfs.NewHdfsClient(c.hdfsAddr)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to get hdfs client: %v", err))
 		}
-	}
-	if err != nil {
-		panic(fmt.Sprintf("Failed to makedir: %v", outdir))
-	}
+		if client != nil {
+			err = client.MkdirAll(outdir, 0775)
+		} else {
+			if !easyfiles.Exists(outdir) {
+				err = easyfiles.Makedirs(outdir)
+			}
+		}
+		if err != nil {
+			panic(fmt.Sprintf("Failed to makedir: %v", outdir))
+		}
 
-	b, err := json.Marshal(&c.Data)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to marshal final results: %v", err))
-	}
-	fpath := filepath.Join(outdir, "missing_loglines.gz")
-	file, err := hdfs.OpenFile(fpath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, easyfiles.GZ_TRUE, client)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to open file: %v", fpath))
-	}
-	defer file.Close()
-	writer, _ := file.Writer(0)
-	defer writer.Close()
-	defer writer.Flush()
+		b, err := json.Marshal(&c.Data)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to marshal final results: %v", err))
+		}
+		fpath := filepath.Join(outdir, "missing_loglines.gz")
+		file, err := hdfs.OpenFile(fpath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, easyfiles.GZ_TRUE, client)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to open file: %v", fpath))
+		}
+		defer file.Close()
+		writer, _ := file.Writer(0)
+		defer writer.Close()
+		defer writer.Flush()
 
-	writer.Write(b)
+		writer.Write(b)
+	*/
 }
 
 func MissingLoglinesMain() {
 	log.SetOutput(ioutil.Discard)
 	env := phonelab.NewEnvironment()
 
-	env.DataCollectors["missing_loglines_collector"] = func() phonelab.DataCollector {
-		c := &MissingLoglinesCollector{}
-		c.Data = make([]*MissingLoglinesResult, 0)
-		return c
-	}
+	/*
+		env.DataCollectors["missing_loglines_collector"] = func() phonelab.DataCollector {
+			c := &MissingLoglinesCollector{}
+			c.Data = make([]*MissingLoglinesResult, 0)
+			return c
+		}
+	*/
 	env.Processors["missing_loglines_processor"] = &MissingLoglinesProcGenerator{}
 
 	conf, err := phonelab.RunnerConfFromFile(os.Args[1])
