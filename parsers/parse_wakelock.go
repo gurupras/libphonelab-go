b2de105ad326835c5b8fbf1f279d6aaff0692af5
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2017-05-03 01:22:34 -0400

    Added a Type() method to ThermaPlanWakeLock

--- parsers/parse_wakelock.go
+++ parsers/parse_wakelock.go
@@ -1,6 +1,10 @@
 package parsers
 
-import "github.com/shaseley/phonelab-go"
+import (
+	"strings"
+
+	"github.com/shaseley/phonelab-go"
+)
 
 type ThermaPlanWakelock struct {
 	Func       string `json:"func"`
@@ -14,6 +18,24 @@ type ThermaPlanWakelock struct {
 
 func (w *ThermaPlanWakelock) New() interface{} {
 	return &ThermaPlanWakelock{}
+}
+
+type WakeLockType int
+
+const (
+	WAKELOCK_UNKNOWN WakeLockType = iota
+	WAKELOCK_ACQUIRE WakeLockType = iota
+	WAKELOCK_RELEASE WakeLockType = iota
+)
+
+func (w *ThermaPlanWakelock) Type() WakeLockType {
+	if strings.Compare("acquireWakeLockInternal", w.Func) == 0 {
+		return WAKELOCK_ACQUIRE
+	} else if strings.Compare("releaseWakeLockInternal", w.Func) == 0 {
+		return WAKELOCK_RELEASE
+	} else {
+		return WAKELOCK_UNKNOWN
+	}
 }
 
 func NewThermaPlanWakelockParser() phonelab.Parser {
