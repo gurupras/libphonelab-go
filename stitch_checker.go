e04855aefb1b345f0a09c17dd9e86bdcf415b547
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2017-05-05 15:36:18 -0400

    Updated stitch_checker.go

--- stitch_checker.go
+++ stitch_checker.go
@@ -1,10 +1,15 @@
 package libphonelab
 
 import (
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"io"
 	"strings"
 
-	"github.com/labstack/gommon/log"
+	"github.com/fatih/set"
 	"github.com/shaseley/phonelab-go"
+	log "github.com/sirupsen/logrus"
 )
 
 type StitchCheckerProcGenerator struct{}
@@ -23,11 +28,17 @@ type StitchCheckerProcessor struct {
 }
 
 type StitchCheckerData struct {
-	DeviceId            string `json:"device_id"`
-	BootId              string `json:"boot_id"`
-	BootIdFail          bool   `json:"boot_id_fail"`
-	IncreasingTokenFail bool   `json:"increasing_token_fail"`
-	LineCount           int64  `json:"line_count"`
+	DeviceId             string `json:"device_id"`
+	BootId               string `json:"boot_id"`
+	BootIdFail           bool   `json:"boot_id_fail"`
+	IncreasingTokenFail  bool   `json:"increasing_token_fail"`
+	DuplicateLoglineFail bool   `json:"duplicate_logline_fail"`
+	LineCount            int64  `json:"line_count"`
+}
+
+func (s *StitchCheckerData) String() string {
+	b, _ := json.Marshal(s)
+	return string(b)
 }
 
 func (p *StitchCheckerProcessor) Process() <-chan interface{} {
@@ -39,6 +50,9 @@ func (p *StitchCheckerProcessor) Process() <-chan interface{} {
 	data.DeviceId = sourceInfo.DeviceId
 	data.BootId = sourceInfo.BootId
 
+	checksums := set.New()
+	ch := make(map[string]struct{})
+
 	var lastLogcatToken int64
 	go func() {
 		defer close(outChan)
@@ -49,6 +63,21 @@ func (p *StitchCheckerProcessor) Process() <-chan interface{} {
 				continue
 			}
 			data.LineCount++
+
+			h := md5.New()
+			io.WriteString(h, ll.Line)
+			checksum := fmt.Sprintf("%x", h.Sum(nil))
+			if _, ok := ch[checksum]; ok {
+				log.Errorf("%v->%v Duplicate logline: \n%v", data.DeviceId, data.BootId, ll.Line)
+				log.Infof("Checksum: %v", checksum)
+				for idx, obj := range checksums.List() {
+					log.Infof("list[%d]=%v", idx, obj)
+				}
+				data.DuplicateLoglineFail = true
+				break
+			}
+			checksums.Add(checksum)
+			ch[checksum] = struct{}{}
 
 			if strings.Compare(ll.BootId, data.BootId) != 0 {
 				if !data.BootIdFail {
